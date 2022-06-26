d235c33d116b162c7244a8741d2d06aeeb52748e
Vincent <vincent.douchin@gmail.com>
2022-06-26 15:52:58 +0200

    favicon and fixed fps engine

--- src/Engine.js
+++ src/Engine.js
@@ -1,12 +1,42 @@
 const Engine = function (uiManager) {
+
+
+    let raf_handle
+    let accumulated_time = 0
+    let current_time = 0
+    let time_step = 1000 / 60
+
+
     let state
     let states
-    const cycle = () => {
-        window.requestAnimationFrame(cycle)
-        state.update()
-        uiManager.update()
-        state.render()
-        uiManager.render()
+    const cycle = (time_stamp) => {
+        raf_handle = window.requestAnimationFrame(cycle)
+
+        accumulated_time += time_stamp - current_time
+        current_time = time_stamp;
+
+        let updated = false
+
+        if (accumulated_time > 60) accumulated_time = time_step
+
+        while (accumulated_time >= time_step) {
+
+            state.update()
+            uiManager.update()
+
+            updated = true
+
+            accumulated_time -= time_step
+
+        }
+
+        if (updated) {
+            state.render()
+            uiManager.render()
+        }
+
+
+
 
 
     }
@@ -24,7 +54,7 @@ const Engine = function (uiManager) {
             states = _states
         },
         start() {
-            cycle()
+            raf_handle = window.requestAnimationFrame(cycle)
         },
         stop() {
             window.cancelAnimationFrame()
